7b8b07c5e99d75109680bd9bfceb14d9166dd520
geovannifranca <geovannidefranca98@gmail.com>
2024-02-23 20:48:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(backgroundColor: Colors.blue)),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Despesas pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: const Card(
+                child: Text('Grafico'),
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          tr.value.toString(),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            tr.title
+                          ),
+                          Text(tr.date.toString())
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
+  }
+}
